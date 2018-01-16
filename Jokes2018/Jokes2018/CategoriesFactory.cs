0da211e8b78b5a1eeb60d6516135ff0cba5642df
Brett <no email>
2018-01-16 09:32:15 -0400

    Before Bonnie's class

---
+++ Jokes2018/Jokes2018/CategoriesFactory.cs
@@ -0,0 +1,47 @@
+﻿using SqlLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokes2018
+{
+    public static class CategoriesFactory
+    {
+
+        public static List<Category> Create()
+        {
+            DataTable dt = ContactSQL.RetrieveAll();
+
+            List<ContactLookup> lstContact = Repackage(dt);
+            return lstContact;
+        }
+
+        private static List<ContactLookup> Repackage(DataTable dt)
+        {
+            List<ContactLookup> list = new List<ContactLookup>();
+
+            if (dt.Rows.Count != 0)
+            {
+                foreach (DataRow dr in dt.Rows)
+                {
+                    ContactLookup myContactLookup = new ContactLookup();
+
+                    myContactLookup.Name = dr["Name"].ToString();
+                    myContactLookup.ContactId = Convert.ToInt32(dr["ContactId"]);
+                    myContactLookup.Email = dr["Email"].ToString();
+                    list.Add(myContactLookup);
+                }
+
+                return list;
+            }
+            else
+            {
+                throw new ArgumentException("No contacts are on file");
+            }
+
+        }
+    }
+}
---
+++ Jokes2018/Jokes2018/Lists.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokes2018
+{
+    public class CategoriesLookup
+    {
+        internal CategoriesLookup() { }
+
+        public int ContactId { get; internal set; }
+        public string Name { get; internal set; }
+        public string Email { get; internal set; }
+
+    }
+}
