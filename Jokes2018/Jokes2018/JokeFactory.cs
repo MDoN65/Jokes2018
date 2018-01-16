7a28f188624a052e5fb7f926fc9c0d2b9cdace8e
Mike <Mike@DESKTOP-IB99P5F>
2018-01-16 09:26:08 -0400

    Finished Joke Factory

---
+++ Jokes2018/Jokes2018/JokeFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlLayer;
+
+namespace Jokes2018
+{
+    public class JokeFactory
+    {
+        public static Joke Create()
+        {
+            return new Joke();
+        }
+
+        public static Joke Create(int jokeId)
+        {
+            DataTable tmpTable = JokeSql.RetrieveJoke(jokeId);
+            return Repackage(tmpTable.Rows[0]);
+        }
+
+        private static Joke Repackage(DataRow myRow)
+        {
+            Joke joke = new Joke();
+            joke._jokeId = Convert.ToInt32(myRow["id"]);
+            joke._title = myRow["title"].ToString();
+            joke._teaser = myRow["teaser"].ToString();
+            joke._jokeText = myRow["joke_text"].ToString();
+            joke._creationDate = Convert.ToDateTime(myRow["created_at"].ToString());
+            joke._userId = Convert.ToInt32(myRow["user_id"]);
+            joke._categoryId = Convert.ToInt32(myRow["category_id"]);
+            joke._isFeatured = Convert.ToBoolean(myRow["postalCode"].ToString());
+
+            return joke;
+        }
+    }
+}
