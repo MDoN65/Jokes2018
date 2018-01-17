f672f7406ac68f0fa7d51a9f609986d98bb4151a
Ryan <no email>
2018-01-17 09:19:04 -0400

    add joke form created

--- Jokes2018/FrontEnd/AddJoke.aspx.designer.cs
+++ Jokes2018/FrontEnd/AddJoke.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace FrontEnd {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// lblMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMessage;
+        
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
@@ -67,31 +76,13 @@ namespace FrontEnd {
         protected global::System.Web.UI.WebControls.Label Label3;
         
         /// <summary>
-        /// txtJoke control.
+        /// txtJokeText control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtJoke;
-        
-        /// <summary>
-        /// Label4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label4;
-        
-        /// <summary>
-        /// txtCreated control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtCreated;
+        protected global::System.Web.UI.WebControls.TextBox txtJokeText;
         
         /// <summary>
         /// Label5 control.
